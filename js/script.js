ad7618a8b2650f5dad81a67aa7e1c8acc23700e3
Camila Zagal <camilazags@gmail.com>
2022-06-09 00:21:10 -0500

    added variables

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let userInput = document.getElementsByClassName("form-control");
+let addBtn = document.getElementsByClassName("add-btn");
+
+//Variables for buttons to save event
+let add8 = document.getElementById("add8");
+let add9 = document.getElementById("add9");
+let add10 = document.getElementById("add10");
+let add11 = document.getElementById("add11");
+let add12 = document.getElementById("add12");
+let add13 = document.getElementById("add13");
+let add14 = document.getElementById("add14");
+let add15 = document.getElementById("add15");
+let add16 = document.getElementById("add16");
+let add17 = document.getElementById("add17");
+let add18 = document.getElementById("add18");
+let add19 = document.getElementById("add19");
+let add20 = document.getElementById("add20");
+let add21 = document.getElementById("add21");
+let add22 = document.getElementById("add22");
+
+//Variables for the input the user enters at each time
+let input8 = document.getElementById("input8");
+let input9 = document.getElementById("input9");
+let input10 = document.getElementById("input10");
+let input11 = document.getElementById("input11");
+let input12 = document.getElementById("input12");
+let input13 = document.getElementById("input13");
+let input14 = document.getElementById("input14");
+let input15 = document.getElementById("input15");
+let input16 = document.getElementById("input16");
+let input17 = document.getElementById("input17");
+let input18 = document.getElementById("input18");
+let input19 = document.getElementById("input19");
+let input20 = document.getElementById("input20");
+let input21 = document.getElementById("input21");
+let input22 = document.getElementById("input22");
